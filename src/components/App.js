98c44c3614d457995882eec859317f174e8b6b0c
Karol <karol.kozer@gmail.com>
2018-06-29 21:42:48 +0200

    add App js

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="container">
+				<h1>Kraken Gifts</h1>
+			</div>
+		);
+	}
+}
