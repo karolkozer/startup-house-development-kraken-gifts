3cd9d10eddf492e2b6c81f86c2d5caa5c926b416
Karol <karol.kozer@gmail.com>
2018-06-30 12:21:39 +0200

    App js
    import components
    add JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+// Import components
+import Header from './Header';
+import Trending from './Trending';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="container">
-				<h1>Kraken Gifts</h1>
+				<Header />
+				<main className="main">
+					<Switch>
+						<Redirect exact from="/" to="/trending" />
+						<Route path="/trending" component={Trending} />
+					</Switch>
+				</main>
 			</div>
 		);
 	}
