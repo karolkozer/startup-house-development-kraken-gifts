fdfb15b93b4fb4b8edefa19e6d5dbeccddd54cce
Karol <karol.kozer@gmail.com>
2018-06-30 16:46:27 +0200

    Trending js
    import Form Sort
    add JSX

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+// Import components
+import FormSort from './Form_Sort';
+
 export default class Trending extends React.Component {
 	render() {
 		return (
 			<section className="section trending">
-				<h1>Trending</h1>
+				<div className="row trending__row">
+					<div className="trending__top">
+						<h2 className="heading-second">Trending</h2>
+						<FormSort />
+					</div>
+				</div>
 			</section>
 		);
 	}
