15d39b5283684060d2260a425508994a8bd5b8e4
Karol <karol.kozer@gmail.com>
2018-06-30 14:08:40 +0200

    add Form Search js

---
+++ src/components/Form_Search.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Import icons
+import icon from '../img/icons/sprite-icons.svg';
+
+export default class FormSearch extends React.Component {
+	render() {
+		return (
+			<form className="form">
+				<input
+					className="form__input"
+					type="text"
+					name="text"
+					id="text"
+					placeholder="Search GIFs"
+					autoComplete="off"
+				/>
+				<button className="btn btn--submit">
+					Find
+					<svg className="icon icon--submit">
+						<use xlinkHref={`${icon}#icon-paper-plane`} />
+					</svg>
+				</button>
+			</form>
+		);
+	}
+}
