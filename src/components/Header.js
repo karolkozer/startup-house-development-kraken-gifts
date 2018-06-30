191dab9db5da157e2d22c9b645ba1e6b5ccdc1a1
Karol <karol.kozer@gmail.com>
2018-06-30 12:42:23 +0200

    Header js
    add header top JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,16 @@ import icon from '../img/icons/sprite-icons.svg';
 const Header = () => {
 	return (
 		<header className="header">
-			<div className="row header__row">
-				<div className="header__logo_box">
-					<div className="header__logo">
-						<svg className="icon icon--logo">
-							<use xlinkHref={`${icon}#icon-google`} />
-						</svg>
+			<div className="header__top">
+				<div className="row header__row">
+					<div className="header__logo_box">
+						<div className="header__logo">
+							<svg className="icon icon--logo">
+								<use xlinkHref={`${icon}#icon-google`} />
+							</svg>
+						</div>
+						<p className="header__logo_name">Kraken</p>
 					</div>
-					<p className="header__logo_name">Kraken</p>
 				</div>
 			</div>
 		</header>
