20c3796e692c0da82e61a1d57931c827363ac11f
Karol <karol.kozer@gmail.com>
2018-06-30 13:00:01 +0200

    Header js
    import Nav
    add JSX header top content

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,24 @@ import React from 'react';
 // Import icons
 import icon from '../img/icons/sprite-icons.svg';
 
+// Import components
+import Nav from './Nav';
+
 const Header = () => {
 	return (
 		<header className="header">
 			<div className="header__top">
 				<div className="row header__row">
-					<div className="header__logo_box">
-						<div className="header__logo">
-							<svg className="icon icon--logo">
-								<use xlinkHref={`${icon}#icon-google`} />
-							</svg>
+					<div className="header__top_content">
+						<div className="header__logo_box">
+							<div className="header__logo">
+								<svg className="icon icon--logo">
+									<use xlinkHref={`${icon}#icon-google`} />
+								</svg>
+							</div>
+							<p className="header__logo_name">Kraken</p>
 						</div>
-						<p className="header__logo_name">Kraken</p>
+						<Nav />
 					</div>
 				</div>
 			</div>
