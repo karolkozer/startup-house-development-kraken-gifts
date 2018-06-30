a1b1cd151fe7bce8c5b3e79ac95a3093291aa808
Karol <karol.kozer@gmail.com>
2018-06-30 12:21:50 +0200

    add Trending js

---
+++ src/components/Trending.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Trending extends React.Component {
+	render() {
+		return (
+			<section className="section trending">
+				<h1>Trending</h1>
+			</section>
+		);
+	}
+}
