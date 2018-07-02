adbb87652156db69a15f89c8574bbe951795ca17
Karol <karol.kozer@gmail.com>
2018-07-02 14:17:23 +0200

    Form Sort js
    import proptypes, mobx
    add contextTypes
    add state
    add change func
    select - add onChange and value

--- src/components/Form_Sort.js
+++ src/components/Form_Sort.js
@@ -1,15 +1,39 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+import { observer, inject } from 'mobx-react';
+
 // Import icons
 import icon from '../img/icons/sprite-icons.svg';
 
-export default class FormSort extends React.Component {
+class FormSort extends React.Component {
+	static contextTypes = {
+		changeSortOptions: PropTypes.func
+	};
+
+	state = {
+		selectValue: ''
+	};
+
+	change = (e) => {
+		const value = e.target.value;
+		this.props.gifs.changeSortOption();
+		this.setState({ selectValue: value });
+	};
+
 	render() {
 		return (
 			<form className="form-sort">
 				<p className="form-sort__text">Sort:</p>
-				<select name="sort" id="sort" className="form-sort__select">
-					<option value="latest" className="form-sort__option" selected>
+
+				<select
+					name="sort"
+					id="sort"
+					className="form-sort__select"
+					onChange={this.change}
+					value={this.state.value}
+				>
+					<option value="latest" className="form-sort__option">
 						latest GIFs
 					</option>
 					<option value="old" className="form-sort__option">
@@ -25,3 +49,5 @@ export default class FormSort extends React.Component {
 		);
 	}
 }
+
+export default inject('gifs')(observer(FormSort));
