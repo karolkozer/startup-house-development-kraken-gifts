598e014e2ff28803714be517b7bb2c974fbcec27
Karol <karol.kozer@gmail.com>
2018-07-02 13:04:25 +0200

    Tending js
    import mobex, proptypes
    add ComponentDidmout
    add mobx store

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,23 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { observer, inject } from 'mobx-react';
 
 // Import components
 import FormSort from './Form_Sort';
-import Gifts from './Gifts';
+import Gifs from './Gifs';
 
-export default class Trending extends React.Component {
+class Trending extends React.Component {
+	static contextTypes = {
+		fetchGifs: PropTypes.func
+	};
+
+	componentDidMount() {
+		// Fetch data
+		this.props.gifs.fetchGifs();
+	}
+
 	render() {
 		return (
 			<section className="section trending">
 				<div className="row trending__row">
 					<div className="trending__top">
 						<h2 className="heading-second">Trending</h2>
-						<FormSort />
+						{/* <FormSort /> */}
 					</div>
 					<div className="trending__bottom">
-						<Gifts />
+						<Gifs />
 					</div>
 				</div>
 			</section>
 		);
 	}
 }
+
+export default inject('gifs')(observer(Trending));
