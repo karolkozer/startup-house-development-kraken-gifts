36462c7a27255aae13bfa7c04bdca7bf701192a7
Karol <karol.kozer@gmail.com>
2018-07-02 13:04:51 +0200

    add helper fetch data js

---
+++ src/helpers/fetch_data.js
@@ -0,0 +1,27 @@
+const PROXY = 'https://cors-anywhere.herokuapp.com/';
+const API_KEY = 'api_key=2VuK9fQrDMgFx57onbKoF3ddB46VYs8Z';
+const BASE_URL = 'http://api.giphy.com/v1/gifs';
+const TRENDING_URL = `${BASE_URL}/trending`;
+
+const status = (response) => {
+	if (response.status >= 200 && response.status < 400) {
+		return Promise.resolve(response);
+	} else {
+		Promise.reject({
+			status: response.status,
+			statusText: response.statusText
+		});
+	}
+};
+
+const fetchData = (url) => {
+	return fetch(url)
+		.then(status)
+		.then((response) => response.json())
+		.catch((error) => alert(Error(`Request filed HTTP status`)));
+};
+
+export const fetchTrending = () => {
+	const url = `${TRENDING_URL}?${API_KEY}`;
+	return fetchData(url);
+};
