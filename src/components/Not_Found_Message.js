e6e8263842e06d5348c88440b29cd8f066e8ff9d
Karol <karol.kozer@gmail.com>
2018-07-03 10:48:37 +0200

    add Not Found Message

---
+++ src/components/Not_Found_Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundMessage = () => (
+	<div className="not-found-message">
+		<h3 className="heading-third">
+			No results found <span className="heading-third__span_icon">;(</span>
+		</h3>
+	</div>
+);
+
+export default NotFoundMessage;
