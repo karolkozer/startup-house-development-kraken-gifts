e6e56766b3f6883dd81f2ea377f7d17cf2813016
Karol <karol.kozer@gmail.com>
2018-07-03 21:59:09 +0200

    Favorites js
    import icons
    update contexTypes
    add addToLibrary func
    add removeFromLibrary  func
    add comments

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,34 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
 
+// Import icons
+import icon from '../img/icons/sprite-icons.svg';
+
 class Favorites extends React.Component {
-	static contextTypes = {};
+	static contextTypes = {
+		gifs: PropTypes.shape({
+			library: PropTypes.object,
+			gifDetails: PropTypes.object,
+			addToLibrary: PropTypes.func,
+			removeFromLibrary: PropTypes.func
+		})
+	};
 
 	addToLibrary = (gif) => {
-		this.props.gifs.addToLibrary(gif);
-		console.log(this.props.gifs.library);
+		const { library, addToLibrary } = this.props.gifs;
+		// Add gif to library
+		addToLibrary(gif);
+		// Update local storage
+		localStorage.setItem('kraken-library', JSON.stringify(library));
 	};
 
-	checkTheLibrary = (check, details) => {
-		console.log('Library', this.props.gifs.library);
-		console.log('Before', details);
-		return !check ? (
+	removeFromLibrary = (gif) => {
+		const { library, removeFromLibrary } = this.props.gifs;
+		// Remove gif from library
+		removeFromLibrary(gif);
+		// Update local storage
+		localStorage.setItem('kraken-library', JSON.stringify(library));
+	};
+
+	// Chek if gif is already in the library, return btn add or remove
+	checkTheLibrary = (check, details) =>
+		!check ? (
 			<button
 				onClick={() => this.addToLibrary(details)}
-				className="btn--controller btn--add"
+				className="btn btn--controller btn--add"
 			>
-				ADD TO FAVORITE
+				<svg className="icon icon--controller">
+					<use xlinkHref={`${icon}#icon-heart-o`} />
+				</svg>
+				Like
 			</button>
 		) : (
-			<button className="btn--controller btn--remove">Removeeeeee</button>
+			<button
+				onClick={() => this.removeFromLibrary(details)}
+				className="btn btn--controller btn--remove"
+			>
+				<svg className="icon icon--controller">
+					<use xlinkHref={`${icon}#icon-heart`} />
+				</svg>
+				Love
+			</button>
 		);
-	};
 
+	// Chek if gif is already in the library
 	checkLibrary = (library, id) =>
 		Object.keys(library).find((gif_id) => gif_id === id);
 
 	render() {
+		// Get data from store
 		const { gifDetails: details, library } = this.props.gifs;
+		// Chek if gif is already in the library in order to render the component with udpated library
 		const check = this.checkLibrary(library, details.id);
 
 		return (
